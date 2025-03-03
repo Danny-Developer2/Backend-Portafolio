7425596083a3ac366009a8246455a4232e5c79be
Juan Luevano Ruiz <daniel_luevanolr@hotmail.com>
2025-03-03 14:34:40 -0600

    Update

--- Backend/API/obj/Debug/net9.0/API.AssemblyInfo.cs
+++ Backend/API/obj/Debug/net9.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d2047a8739f0145a2ae9d203595ca2065370185b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+17bb76ace311a4c4c38c75f582a318f2ee9813a0")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
